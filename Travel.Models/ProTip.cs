e9ceaa21764a0171dd15b3205378d6135f5af4be
MK <searedahi@outlook.com>
2019-01-08 19:59:55 -0800

    'CommitedFromBATModels'

---
+++ Travel.Models/ProTip.cs
@@ -0,0 +1,17 @@
+﻿using Travel.Models;
+
+namespace TravelMinded.Models
+{
+    public class ProTip : IBaseTemplate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ShortName { get; set; }
+
+        //Navigation properties
+        public IExperience Experience { get; set; }
+        public IProduct Product { get; set; }
+
+    }
+}
